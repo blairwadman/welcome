20757b50de71c9bae8af941a641e9135caf014ee
Blair Wadman <blair@wadmanconsulting.com>
2017-02-04 11:32:09 +0000

    Made the block content configurable

--- src/Plugin/Block/WelcomeBlock.php
+++ src/Plugin/Block/WelcomeBlock.php
@@ -3,19 +3,51 @@
 namespace Drupal\welcome\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
+/**
+ * Provides a 'Welcome' Block
+ *
+ * @Block(
+ *   id = "welcome_block",
+ *   admin_label = @Translation("Welcome block"),
+ * )
+ */
 class WelcomeBlock extends BlockBase {
   /**
-   * Provides a 'Welcome' Block
-   *
-   * @Block(
-   *   id = "welcome_block",
-   *   admin_label = @Translation("Welcome block"),
-   * )
+   * {@inheritdoc}
    */
   public function build() {
     $name = \Drupal::currentUser()->getDisplayName();
     return array(
-      '#markup' => $this->t('Hello @name, thank you for logging in and welcome!', array('@name' => $name)),
+      '#markup' => $this->t('@welcome_message', array('@welcome_message' => $this->configuration['welcome_message'])),
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'welcome_message' => 'Hello world',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $form['welcome_message'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Welcome message'),
+      '#default_value' => $this->configuration['welcome_message'],
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $this->configuration['welcome_message'] = $form_state->getValue('welcome_message');
   }
 }
